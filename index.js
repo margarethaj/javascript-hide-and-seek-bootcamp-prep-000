1341230f5a8a6d5183c4c45e9ed7c16870618d3e
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-22 23:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function deepestChild(){
     doc[i].innerHTML = (i+1).toString();
   }*/
   console.log(doc[doc.length-1].innerHTML);
-  return (doc[doc.length-1].innerHTML);
+  return (doc[doc.length-1]);
 }
