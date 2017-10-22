edad691cdc402d4fda1c44d2801f13934b6b9d56
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-22 23:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ function nestedTarget(){
 
 function deepestChild(){
   const doc = document.getElementById('grand-node').querySelectorAll('div');
-  /*for(let i=0; i<doc.length; i++){
-    doc[i].innerHTML = (i+1).toString();
-  }*/
   console.log(doc[doc.length-1].innerHTML);
   return (doc[doc.length-1]);
 }
