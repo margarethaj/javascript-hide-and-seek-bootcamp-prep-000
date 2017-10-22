6518a162d7073fa8d53fd955ae8759719a613079
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-22 23:54:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+ }
+
+function nestedTarget(){
+ return document.getElementById('nested').querySelector('.target');
+}
+
+function deepestChild(){
+  const doc = document.getElementById('grand-node').querySelectorAll('div');
+  console.log(doc[doc.length-1].innerHTML); //inside the div
+  return (doc[doc.length-1]); //include the div
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  for(let i=0; i<rankedLists.length; i++){
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
